e39c4666508b52b21e9ef6cf8368de3b30f46e53
Nick Stupich <nick.stupich@gmail.com>
2012-05-14 23:07:39 -0400

    starting on data protocol class

--- Computer2/Computer2/DataProtocol/DataProtocol.cpp
+++ Computer2/Computer2/DataProtocol/DataProtocol.cpp
@@ -1,6 +1,29 @@
 #include "DataProtocol.h"
 
+#define CONTROL_BYTE	255
+#define DATA_LENGTH		8
+
+enum ExpectedNext{
+	CONTROL,
+	GAIN,
+	DATA,
+	FAILURE
+};
+
+#define ONES_IN_BYTE(x)	(x&(1<<7)) + (x&(1<<6)) + (x&(1<<5)) + (x&(1<<4)) + (x&(1<<3)) + (x&(1<<2)) + (x&(1<<1)) + (x&1)
+
+DataProtocol::DataProtocol(unsigned int channels, DataListener* dataListener)
+{
+	this->_channels = channels;
+	this->_numChannels = ONES_IN_BYTE(channels);
+
+	this->_gains = new unsigned int[this->_channels];
+	this->_rawData = new unsigned int*[this->_channels];
+	this->_cleanData = new unsigned int*[this->_channels];
+}
+
 void DataProtocol::AddByte(unsigned char n)
 {
 	LOG_DEBUG("Received byte");
 }
+
--- Computer2/Computer2/DataProtocol/DataProtocol.h
+++ Computer2/Computer2/DataProtocol/DataProtocol.h
@@ -2,11 +2,27 @@
 #define _IDATAPROTOCOL
 
 #include "../Utils/Log.h"
+#include "DataListener.h"
+#include "Serial.h"
 
 class DataProtocol{
 public:
+	DataProtocol(unsigned int channels, DataListener* dataListener);
+	int start();
+	int stop();
+	~DataProtocol();
 	void AddByte(unsigned char n);
+	unsigned int** GetData();
 
+private:
+	unsigned int* _gains;
+	unsigned int** _rawData;
+	unsigned int** _cleanData;
+	unsigned int _channels;
+	unsigned int _numChannels;
+
+	Serial* _serial;
+	DataListener* _dataListener;
 };
 
 #endif
