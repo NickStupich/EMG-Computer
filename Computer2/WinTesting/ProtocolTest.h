c5451521c6ebd774111a4e9e553fada2131fa96a
Nick Stupich <nick.stupich@gmail.com>
2012-05-17 10:21:51 -0400

    a little more testing of the synchronization stuff, still seems solid

--- Computer2/WinTesting/ProtocolTest.h
+++ Computer2/WinTesting/ProtocolTest.h
@@ -4,15 +4,17 @@
 
 class TestDataListener : public DataListener{
 public:
-	int dataCount;
+	int dataCount, delay;
 
-	TestDataListener()
+	TestDataListener(int delayMs)
 	{
+		this->delay = delayMs;
 		this->dataCount = 0;
 	}
 
 	void OnNewData(unsigned int** data) override
 	{
+		Sleep(this->delay);
 		printf("got data\n");
 		this->dataCount++;
 	}
@@ -27,8 +29,8 @@ void testProtocol()
 {
 	printf("Testing data protocol\n\n");
 	int response;
-	
-	/*DataProtocol* dp1 = new DataProtocol(1, NULL);
+
+	DataProtocol* dp1 = new DataProtocol(1, NULL);
 	response = dp1->Start();
 
 	if(response != R_SUCCESS)
@@ -39,8 +41,10 @@ void testProtocol()
 
 	if(response != R_SUCCESS)
 		printf("Failed test2\n");
-		*/
-	TestDataListener* dl = new TestDataListener();
+
+	Sleep(500);
+		
+	TestDataListener* dl = new TestDataListener(0);
 	DataProtocol* dp2 = new DataProtocol(1, (DataListener*)dl);
 	response = dp2->Start();
 	if(response != R_SUCCESS)
@@ -53,6 +57,23 @@ void testProtocol()
 		printf("failed test4\n");
 
 	printf("Number of data updates to the listener: %d\n", dl->dataCount);
+	
+	TestDataListener* dl2 = new TestDataListener(1000);
+	DataProtocol* dp3 = new DataProtocol(1, (DataListener*)dl2);
+	response = dp3->Start();
+	if(response != R_SUCCESS)
+		printf("failed test3\n");
+
+	Sleep(5000);
+
+	response = dp3->Stop();
+	if(response != R_SUCCESS)
+		printf("failed test4\n");
+
+	printf("received %d updates\n", dl2->dataCount);
+	if(dl2->dataCount > 6)
+		printf("received too many updates...");
+
 
 	printf("Done data protocol test\n");
 }
